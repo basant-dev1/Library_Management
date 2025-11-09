68415735a9ef32d94f21c04d1ecd1e164048f1c2
Basant <official.kbasant.me@gmail.com>
2025-11-10 01:48:55 +0530

    Fix controller import path case

--- App/routes/bookRoutes.js
+++ App/routes/bookRoutes.js
@@ -1,8 +1,9 @@
 // App/routes/bookRoutes.js
 const express = require("express");
+const router = express.Router();
 const { BookList, InsertBook } = require("../controllers/bookDetails_Controller");
 
-const router = express.Router();
+
 
 router.get("/book-list", BookList);
 router.post("/Insert-Book", InsertBook);
